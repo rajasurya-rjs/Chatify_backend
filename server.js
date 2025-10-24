28b7437454a4dfbcd524d85e245481654474361f
Rajasurya <rajasurya2006rjs@gmail.com>
2025-10-24 19:04:46 +0530

    Fix server port and ready for deployment

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import path from "path";
 import cors from "cors";
 
 import authRoutes from "./src/routes/auth.route.js";
@@ -9,27 +8,21 @@ import { connectDB } from "./src/lib/db.js";
 import { ENV } from "./src/lib/env.js";
 import { app, server } from "./src/lib/socket.js";
 
-const __dirname = path.resolve();
-
 const PORT = ENV.PORT || 3000;
 
-app.use(express.json({ limit: "5mb" })); // req.body
+// Middleware
+app.use(express.json({ limit: "5mb" }));
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use(cookieParser());
 
+// API routes
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// make ready for deployment
-if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// **Remove frontend static serving** because frontend is deployed separately
 
-  app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
-
-server.listen(PORT, () => {
+// Start server & connect DB
+server.listen(PORT, async () => {
   console.log("Server running on port: " + PORT);
-  connectDB();
+  await connectDB();
 });
